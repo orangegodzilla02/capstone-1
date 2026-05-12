28ab16080bed090f8e8332dff69c7d12d7369718
Sam Williams <orangegodzilla02@gmail.com>
2026-05-12 10:05:34 -0500

    Implement 3n + 1 sequence logic in script.js
    Add functions to calculate and display the maximum value and number of steps in the 3n + 1 sequence.

--- script.js
+++ script.js
@@ -6,3 +6,60 @@ function p4T() {
    p4.style.display = "none";
  }
 }
+
+function getStartingValue() {
+  let minText = document.getElementById('min').value;
+  let startingvalue = parseInt(minText);
+
+  return startingvalue
+}
+
+function checkvalue() {
+
+  let minText = document.getElementById('min').value;
+  let startingvalue = parseInt(minText);
+  console.log("StartingValue: " + startingvalue);
+  
+}
+
+function run() {
+  startingValue = getStartingValue();
+
+ const steps = [startingValue];
+
+  let maxvalue = startingValue;
+  let result = threeNPlusOne(startingValue); 
+
+  while (result !== 1) {
+    if (result > maxvalue) {
+      maxvalue = result;
+    }
+    steps.push(result);
+    let nextvalue = threeNPlusOne(result);
+    result = nextvalue;
+  }
+  console.log ("maxvalue " + maxvalue);
+  steps.push(result);
+  console.log (steps);
+  let numsteps = steps.length;
+  document.getElementById("numsteps").innerText = "most steps: " + numsteps;
+  document.getElementById("maxvalue").innerText = "max value: " + maxvalue;
+  
+}
+
+function threeNPlusOne(startingValue) {
+  console.log(startingValue);
+  let checkedvalue = Number((startingValue) % (2));
+  console.log (checkedvalue);
+
+  let result = startingValue % 2;
+
+  if (result === 1) {
+    let newvalue = (startingValue * 3) + 1;
+    return newvalue
+  } else {
+    let newvalue = (startingValue / 2);
+    return newvalue
+ }
+  
+}
