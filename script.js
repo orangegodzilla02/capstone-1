e629168e5006a38a8788d3fc9cd9adc480485b92
Sam Williams <orangegodzilla02@gmail.com>
2026-05-14 14:47:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,13 @@ function getStartingValue() {
   let minText = document.getElementById('min').value;
   let startingvalue = parseInt(minText);
 
-  return startingvalue
+  return BigInt(startingvalue);
+}
+
+function getEndingValue() {
+   let endingtext = document.getElementById('max').value;
+  let endingValue = parseInt(endingtext);
+  return BigInt(endingValue);
 }
 
 function checkvalue() {
@@ -22,15 +28,21 @@ function checkvalue() {
   
 }
 
-function run() {
-  startingValue = getStartingValue();
+function runAll() {
+   startingValue = getStartingValue();
+   endingValue = getEndingValue();
+  for (let index = startingValue; index < endingValue; index++) {
+    run(index);
+  }
+}
+function run(startingValue) {
 
  const steps = [startingValue];
 
   let maxvalue = startingValue;
   let result = threeNPlusOne(startingValue); 
 
-  while (result !== 1) {
+  while (result !== 1n) {
     if (result > maxvalue) {
       maxvalue = result;
     }
@@ -38,28 +50,28 @@ function run() {
     let nextvalue = threeNPlusOne(result);
     result = nextvalue;
   }
+
+  
   console.log ("maxvalue " + maxvalue);
   steps.push(result);
   console.log (steps);
   let numsteps = steps.length;
   document.getElementById("numsteps").innerText = "most steps: " + numsteps;
-  document.getElementById("maxvalue").innerText = "max value: " + maxvalue;
+  document.getElementById("maxvalue").innerText = "max value: " + maxvalue.toLocaleString();
   
 }
 
 function threeNPlusOne(startingValue) {
   console.log(startingValue);
-  let checkedvalue = Number((startingValue) % (2));
-  console.log (checkedvalue);
 
-  let result = startingValue % 2;
+  let result = startingValue % 2n;
 
-  if (result === 1) {
-    let newvalue = (startingValue * 3) + 1;
-    return newvalue
+  if (result === 1n) {
+    let newvalue = (startingValue * 3n) + 1n;
+    return BigInt(newvalue);
   } else {
-    let newvalue = (startingValue / 2);
-    return newvalue
+    let newvalue = (startingValue / 2n);
+    return BigInt(newvalue);
  }
   
 }
